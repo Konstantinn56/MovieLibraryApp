65ad9fe70caac217c2802effaed6fe8d133c1ff3
Velina666 <velina_yordanova004@abv.bg>
2022-03-15 17:36:26 +0200

    AccountTests

--- MovieLibrary/UnitTests/AccountTests.cs
+++ MovieLibrary/UnitTests/AccountTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Data.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests
 {
@@ -8,7 +9,14 @@ namespace UnitTests
         [TestMethod]
         public void Account()
         {
+            Account account = new Account();
+            account.FirstName = "Dimitar";
+            account.LastName = "Georgiev";
+            account.Username = "zero123";
+            account.Password = "z123456";
+            account.RecoveryKey = 1234;
 
+            Assert.IsInstanceOfType(account, typeof(Account));
         }
     }
 }
