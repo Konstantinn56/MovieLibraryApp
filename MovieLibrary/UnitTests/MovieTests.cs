6342dff1f8ba423edebee200ead0dc5747a2431e
Velina666 <velina_yordanova004@abv.bg>
2022-03-15 16:57:11 +0200

    MovieTests

---
+++ MovieLibrary/UnitTests/MovieTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MovieTests
+    {
+        [TestMethod]
+        public void Movie()
+        {
+
+        }
+    }
+}
