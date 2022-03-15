8057ed93570d9eec629a020ca88c807342bfe95e
Velina666 <velina_yordanova004@abv.bg>
2022-03-15 16:42:58 +0200

    AccountBusinessTests

---
+++ MovieLibrary/UnitTests/AccountBusinessTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AccountBusinessTests
+    {
+            [TestMethod]
+            public void AddAccount()
+            {
+
+            }
+
+            [TestMethod]
+            public void DeleteAccount()
+            {
+
+            }
+
+            [TestMethod]
+            public void UpdateAccount()
+            {
+
+            }
+    }
+}
