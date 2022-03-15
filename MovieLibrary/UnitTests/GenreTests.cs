feaf2f2c1d6caad464d76fdc74c4859c0fdd241e
Velina666 <velina_yordanova004@abv.bg>
2022-03-15 16:55:58 +0200

    GenreTests

---
+++ MovieLibrary/UnitTests/GenreTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class GenreTests
+    {
+        [TestMethod]
+        public void Genre()
+        {
+
+        }
+    }
+}
