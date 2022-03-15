d91c0937018b39c4ce7edc4058f58427c3e0283e
Velina666 <velina_yordanova004@abv.bg>
2022-03-15 16:54:24 +0200

    AccountTests

---
+++ MovieLibrary/UnitTests/AccountTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public void Account()
+        {
+
+        }
+    }
+}
