19b51a544faae95e499ffc8cc43b58b6ab0287a0
Velina666 <velina_yordanova004@abv.bg>
2022-03-15 18:37:38 +0200

    GenreBusinessTests

--- MovieLibrary/UnitTests/GenreBusinessTests.cs
+++ MovieLibrary/UnitTests/GenreBusinessTests.cs
@@ -2,15 +2,24 @@
 
 namespace UnitTests
 {
+    /// <summary>
+    /// Test for GenreBusiness
+    /// </summary>
     [TestClass]
     public class GenreBusinessTests
     {
+        /// <summary>
+        /// Test for method Add
+        /// </summary>
         [TestMethod]
         public void AddGenre()
         {
 
         }
 
+        /// <summary>
+        /// Test for method Get
+        /// </summary>
         [TestMethod]
         public void GetGenre()
         {
