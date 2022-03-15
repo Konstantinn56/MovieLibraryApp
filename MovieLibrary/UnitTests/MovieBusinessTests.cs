5d87104e74947872ee20ee6cc4d77e6e4e3d87a4
Velina666 <velina_yordanova004@abv.bg>
2022-03-15 18:36:03 +0200

    MovieBusiness

--- MovieLibrary/UnitTests/MovieBusinessTests.cs
+++ MovieLibrary/UnitTests/MovieBusinessTests.cs
@@ -2,27 +2,42 @@
 
 namespace UnitTests
 {
+    /// <summary>
+    /// Test for MovieBusiness
+    /// </summary>
     [TestClass]
     public class MovieBusinessTests
     {
+        /// <summary>
+        /// Test for method Get
+        /// </summary>
         [TestMethod]
         public void GetMovie()
         {
 
         }
 
+        /// <summary>
+        /// Test for method Add
+        /// </summary>
         [TestMethod]
         public void AddMovie()
         {
 
         }
 
+        /// <summary>
+        /// Test for method Delete
+        /// </summary>
         [TestMethod]
         public void DeleteMovie()
         {
 
         }
 
+        /// <summary>
+        /// Test for method Update
+        /// </summary>
         [TestMethod]
         public void UodateMovie()
         {
